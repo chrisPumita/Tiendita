b4804ba6863847396c875b7905e08afa61b96eff
Christan RCSG <christian.floppy@gmail.com>
2017-06-01 12:37:31 -0500

    Lecutra de .dat
    Codigo para leer archivos .dat

--- productos.c
+++ productos.c
@@ -2,10 +2,31 @@
 #include <string.h>
 #include "productos.h"
 #include "Bool.h"
+#define TAM_MAX 50
+#define TAM_BAR 20
 
-	Producto crearProducto(int, char);
+void leerProducto(FILE * arch, Producto* reg)
+{
+	fread(reg,sizeof(Producto),1,arch);
+}
+
+void grabaProdudcto(FILE* arch, Producto* reg)
+{
+	fwrite(reg,sizeof(Producto),1,arch);
+}
+
+/*
+
+DesplegasStock()
+{
+}
+ */
+
+
 int main(int argc, char const *argv[])
 {
+	
+	/*
 	FILE* arch;
 	Producto a;
 
@@ -13,15 +34,44 @@ int main(int argc, char const *argv[])
 	arch = fopen("lista-de-precios.dat", "r+b");
 
 	//La primera leida la hacemos fuera del while
-	//fread(&a,sizeof(Producto),1,arch);
-	printf("ID  | CODIGO |  NOMBRE |   $   |  Stock |\n");
+	printf("ID\t| CODIGO |  NOMBRE |   $   |  Stock |\n");
+	leerProducto(arch,&a);
 	while(!feof(arch))
 	{
-		printf("%d-\t%s-\t%s%.2f\t%d\n",a.indice,a.barCode,a.nombre,a.cUni,a.stock);
+		printf("%d\t",a.indice);
+		printf("%s\t",a.barCode);
+		printf("%s\t",a.nombre);
+		printf("%.2f\t",a.cUni);
+		printf("%d\t",a.stock);
+		printf("\n");
 		//leemos el sig registro del archivo
-		fread(&a,sizeof(Producto),1,arch);
+//		fread(&a,sizeof(Producto),1,arch);
+		leerProducto(arch,&a);
 	}
 
 	fclose(arch);
+	 */
+
+	FILE* arch = fopen("lista-de-precios.dat","r+");
+	int ID;
+	char barCode[TAM_BAR];
+	char nombre[TAM_MAX];
+	float cUni;
+	int stock;
+
+	fscanf(arch,"%d %s %s %f %d",&ID,barCode,nombre,&cUni,&stock);
+	while(!feof(arch)!= '\0')
+	{
+		printf("%d\t",ID);
+		printf("%s\t",barCode);
+		printf("%s\t",nombre);
+		printf("%.2f\t",cUni);
+		printf("%d\t",stock);
+		printf("\n");
+
+		fscanf(arch,"%d %s %s %f %d",&ID,barCode,nombre,&cUni,&stock);
+	}
+	fclose(arch);
+
 	return 0;
 }
