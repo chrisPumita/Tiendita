c2a0bb3aedb317d80b78899976b434f6a2d46925
Christan RCSG <christian.floppy@gmail.com>
2017-06-01 12:44:56 -0500

    archivo .h

--- productos.h
+++ productos.h
@@ -10,3 +10,15 @@
  };
  typedef struct Producto_Type Producto;
 
+#include <stdio.h>
+#include <string.h>
+#include "productos.h"
+#include "Bool.h"
+#define TAM_MAX 50
+#define TAM_BAR 20
+
+void leerProducto(FILE * arch, Producto* reg);
+
+void grabaProdudcto(FILE* arch, Producto* reg);
+
+void DesplegasStock();
